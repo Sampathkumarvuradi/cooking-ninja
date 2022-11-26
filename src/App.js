ec1fe4039c4c6f2f17699bc972deafb3c3bd585e
Sampath Kumar <sampathkumarvuradi@gmail.com>
2022-11-26 21:00:21 +0530

    Added Pages and Initiated Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// page components
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
+
+// styles
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/recipe/:id' element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
